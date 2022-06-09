0831ba0fab6931f47ca709f62de9a0ce8973e997
Nicolas Ouagne <n.ouagne@betclicgroup.com>
2022-06-09 14:57:49 +0200

    commit service

---
+++ Poker.Grpc/Services/GameService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace Poker.Grpc.Services
+{
+    public class GameService : Game.GameBase
+    {
+        public override Task<StartGameResponse> StartGame(StartGameRequest request, ServerCallContext context)
+        {
+            var response = new StartGameResponse();
+            return response;
+
+
+        }
+    }
+}
--- Poker.Grpc/Startup.cs
+++ Poker.Grpc/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Poker.Grpc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,7 +32,7 @@ namespace Poker.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<GameService>();
 
                 endpoints.MapGet("/", async context =>
                 {
