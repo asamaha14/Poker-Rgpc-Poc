3524dc8f3542315424e277db18418955b529fcb6
Abbass SAMAHA <a.samaha@betclicgroup.com>
2022-06-09 15:14:27 +0200

    Added mapping for players

--- Poker.Grpc/Services/GameService.cs
+++ Poker.Grpc/Services/GameService.cs
@@ -1,5 +1,9 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf.Collections;
+using Grpc.Core;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using TexasHoldem.Logic.GameMechanics;
+using TexasHoldem.Logic.Players;
 
 namespace Poker.Grpc.Services
 {
@@ -7,10 +11,40 @@ namespace Poker.Grpc.Services
     {
         public override Task<StartGameResponse> StartGame(StartGameRequest request, ServerCallContext context)
         {
+            // Call Game
+            var players = new List<IPlayer>();
+
+            players.Add(new InnovationDayPlayer("ABS"));
+            players.Add(new InnovationDayPlayer("Fatma"));
+            players.Add(new InnovationDayPlayer("Nico"));
+
+            var game = new TexasHoldemGame(players);
+            var result = game.StartGameCustom();
+
+
+            // Map Response
             var response = new StartGameResponse();
-            return response;
+            RepeatedField<Player> playersToReturn = new RepeatedField<Player>();
 
+            foreach (var tempPlayer in result)
+            {
+                var player = this.MapToPlayer(tempPlayer);
+                playersToReturn.Add(player);
+            }
 
+            response.Players.Add(playersToReturn);
+            return Task.FromResult(response);
         }
+
+        private Player MapToPlayer(IPlayer player)
+        {
+            return new Player()
+            {
+                Name = player.Name,
+                BuyIn = player.BuyIn,
+
+            };
+        }
+
     }
 }
